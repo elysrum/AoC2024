584783268b625eb93d64bc94bc65f8afcad63eff
elysrum <github@nettorrent.com>
2024-12-05 08:10:30 +0000

    Day 5 prep

---
+++ challenge/day5/a_test.go
@@ -0,0 +1,44 @@
+package day5
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestA(t *testing.T) {
+
+	input := strings.NewReader(`47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`)
+
+	result := partA(input)
+
+	require.Equal(t, 18, result)
+}
