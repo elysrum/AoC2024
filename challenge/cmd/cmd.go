e549607fa6929a0e1607216760f5a855d28e527d
elysrum <git@nettorrent.com>
2024-12-12 15:23:54 +0000

    Day 13 Prep

--- challenge/cmd/cmd.go
+++ challenge/cmd/cmd.go
@@ -22,7 +22,7 @@ import (
 	"AoC2024/challenge/day10"
 	"AoC2024/challenge/day11"
 	"AoC2024/challenge/day12"
-	// "AoC2024/challenge/day13"
+	"AoC2024/challenge/day13"
 	// "AoC2024/challenge/day14"
 	// "AoC2024/challenge/day15"
 	// "AoC2024/challenge/day16"
@@ -52,7 +52,7 @@ func addDays(root *cobra.Command) {
 	day10.AddCommandsTo(root)
 	day11.AddCommandsTo(root)
 	day12.AddCommandsTo(root)
-	// day13.AddCommandsTo(root)
+	day13.AddCommandsTo(root)
 	// day14.AddCommandsTo(root)
 	// day15.AddCommandsTo(root)
 	// day16.AddCommandsTo(root)
---
+++ challenge/day13/a_test.go
@@ -0,0 +1,17 @@
+package day13
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestA(t *testing.T) {
+
+	input := strings.NewReader(`125 17`)
+
+	result := partA(input)
+
+	require.Equal(t, 55312, result)
+}
